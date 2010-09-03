b0bd7882f553aca3af24beb82f06f91d3ca72a20
Babar <babar@cern.ch>
2010-09-03 10:35:16 +0200

    Fix typo user => owner

--- chef/recipes/scientific.rb
+++ chef/recipes/scientific.rb
@@ -16,7 +16,7 @@ cookbook_file "/etc/selinux/targeted/modules/active/modules/chef.pp" do
 end
 
 cookbook_file "/usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb" do
-  user "root"
+  owner "root"
   group "root"
   mode "0644"
   not_if "grep -q scientific /usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb"
