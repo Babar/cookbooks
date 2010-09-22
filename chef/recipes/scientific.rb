6c4cc494991c768d985ff613f86dc91b007cdca8
Babar <babar@cern.ch>
2010-09-22 16:54:57 +0200

    Properly does this test... reload => false, so no reload!

--- chef/recipes/scientific.rb
+++ chef/recipes/scientific.rb
@@ -1,5 +1,12 @@
 # Ensure chef knows the scientific platform
+cookbook_file "/usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb" do
+  owner "root"
+  group "root"
+  mode "0644"
+  not_if "grep -q scientific /usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb"
+end
 
+# Also ensure SElinux permissions are OK
 service "chef-client" do
   supports :status => true, :restart => true, :reload => false
   enabled true
@@ -11,12 +18,5 @@ cookbook_file "/etc/selinux/targeted/modules/active/modules/chef.pp" do
   owner "root"
   group "root"
   not_if do File.exists?("/etc/selinux/targeted/modules/active/modules/chef.pp") end
-  notifies :reload, resources(:service => "chef-client"), :immediately
-end
-
-cookbook_file "/usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb" do
-  owner "root"
-  group "root"
-  mode "0644"
-  not_if "grep -q scientific /usr/lib/ruby/gems/1.8/gems/chef-0.9.8/lib/chef/platform.rb"
+  notifies :restart, resources(:service => "chef-client")
 end
